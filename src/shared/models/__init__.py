909807aaff246cf1badfe031804aedd7e970dbdd
Valentin Gagarin <valentin.gagarin@tweag.io>
2026-02-02 12:41:15 +0100

    chore: satisfy stricter linter requirements

--- src/shared/models/__init__.py
+++ src/shared/models/__init__.py
@@ -1,5 +1,19 @@
 from .cached import *
 from .cve import *
 from .issue import *
-from .linkage import *
+from .linkage import (
+    CVEDerivationClusterProposal,
+    DerivationClusterProposalLink,
+    MaintainersEdit,
+    PackageEdit,
+    ProvenanceFlags,
+)
 from .nix_evaluation import *
+
+__all__ = [
+    "CVEDerivationClusterProposal",
+    "MaintainersEdit",
+    "PackageEdit",
+    "ProvenanceFlags",
+    "DerivationClusterProposalLink",
+]
