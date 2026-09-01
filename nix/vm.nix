fee4353412897446cc249edba3fa5ce88cc678c5
Valentin Gagarin <valentin.gagarin@tweag.io>
2026-09-01 08:57:43 +0200

    feat: expose web server on the host

--- nix/vm.nix
+++ nix/vm.nix
@@ -1,9 +1,29 @@
-{ modulesPath, ... }:
+{
+  modulesPath,
+  config,
+  ...
+}:
 {
   imports = [ (modulesPath + "/virtualisation/qemu-vm.nix") ];
 
   networking.hostName = "sectracker-local";
-  services.getty.autologinUser = "root";
+
+  services = {
+    getty.autologinUser = "root";
+
+    nginx = {
+      enable = true;
+      virtualHosts._.default = true;
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ config.services.nginx.defaultHTTPListenPort ];
+
+  local = {
+    port-offset = 50000;
+    ports.nginx = config.services.nginx.defaultHTTPListenPort;
+  };
+
   virtualisation.graphics = false;
   system.stateVersion = "24.05";
 }
